726f4dc29c5b2606ca77363124842fe2c612abf5
Lubimec_ <dowslf@gmail.com>
2023-03-18 16:15:24 +0500

    lower back still hurts

---
+++ MOFUI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MOFUI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOFUI.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+    }
+}
