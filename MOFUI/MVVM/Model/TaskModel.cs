09ae7bb958d768f01cd7e72680d8cdd8e6a58b65
Lubimec_ <dowslf@gmail.com>
2023-04-05 23:38:29 +0500

    я даун

---
+++ MOFUI/MVVM/Model/TaskModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOFLibrary
+{
+    /// <summary>
+    /// Represents small tasks 
+    /// inside the main quest.
+    /// </summary>
+    internal class TaskModel
+    {
+        public string TaskArticle { get; set; }
+        public string TaskDescription { get; set; }
+        [DefaultValue("Unknown")]
+        public string TaskLocation { get; set; }
+    }
+}
---
+++ MOFlib/QuestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOFLibrary
+{
+    /// <summary>
+    /// Represents the template
+    /// of the main quest
+    /// </summary>
+    internal class QuestModel
+    {
+        public Guid id { get; set; }
+        public string QuestName { get; set; }
+        public List<TaskModel> Quest { get; set; } = new List<TaskModel>();
+        public string QuestRequester { get; set; }
+        public string QuestExecuter { get; set;}
+        public DateTime? QuestDeadline { get; set; }
+    }
+}
